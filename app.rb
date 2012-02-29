16d2ab8987b7ff4f81c5165c15d364ee44334db7
supamanu <etabarly@gmail.com>
2012-02-29 22:38:19 +0100

    completed the model for (almost?) all tables

--- app.rb
+++ app.rb
@@ -37,19 +37,32 @@ end
 # homepage route for adding new notes using POST verb
 post '/' do
   n=Autruche.new
-  #the parameter :content is the name of the <textarea> defined in the form
+  #the :parameter is the name of the <textarea> defined in the form
   #this is how you can target what field to take from the form
   #and us in your database model
   n.nom = params[:nom]
   n.prenom = params[:prenom]
   n.callsign = params[:callsign]
 
-#  n.created_at = Time.now
- 
   if n.save
     flash[:notice] = "Pilote ajout&eacute &agrave la base"
   else
     flash[:error] = "Operation echouee. On est dans la merde"
   end
   redirect '/'
+end
+
+# admin route for campaign manager
+get '/admin' do
+  @campagnes=Campagne.all :order=>:id.desc
+  @title='Gestion Campagne'
+  erb :admin
+end
+
+post '/admin' do
+  n=Campagne.new
+  n.nom = params[:nom]
+  n.descriptif = params[:descriptif]
+  n.save
+  redirect '/admin'
 end
--- models.rb
+++ models.rb
@@ -28,8 +28,14 @@ class Avatar
   property :created_at,  DateTime
   
   belongs_to :autruche
+  
   has n, :flights
-  has n, :missions, :through => :flights
+  has n, :promotions
+  has n, :rewards
+  
+  has n, :missions,    :through => :flights
+  has n, :grades,      :through => :promotions
+  has n, :decorations, :through => :rewards
  
 end
 
@@ -54,13 +60,43 @@ class Mission
   property :nom,        String,  :required => true
   property :briefing,   Text,    :required => false, :default => "Pas de briefing"
   property :debriefing, Text,    :required => false, :default => "Pas de debriefing"
+  property :date_hist,  DateTime,:required => false
   property :created_at,  DateTime
   
   belongs_to :campagne
+
   has n, :flights
   has n, :avatars, :through => :flights
  
 end
+
+class Decoration
+  include DataMapper::Resource
+  property :id,       Serial
+  property :nom,      Text, :required => true
+  #property :image,    Binary
+  
+  has 1, :nation
+  
+  has n, :rewards
+  has n, :avatars, :through => :rewards
+end
+
+class Grade
+  include DataMapper::Resource
+  property :id,       Serial
+  property :nom,      Text, :required => true
+  #property :image,    Binary
+  
+  has 1, :nation
+  
+  has n, :promotions
+  has n, :avatars, :through =>:promotions
+end
+
+###################
+### JOIN TABLES ###
+###################
 
 # Flight model - defines the join between avatars and missions
 # use it to store meta-data about when an avatar went on a mission
@@ -70,9 +106,81 @@ class Flight
   property :created_at,  DateTime
   property :updated_at,  DateTime
   
-  belongs_to :avatar, :key => true
-  belongs_to :mission, :key => true 
+  belongs_to :avatar,  :key => true
+  belongs_to :mission, :key => true
+  has 1, :monture
 end
+
+# Promotion model - defines the join between avatars and grades
+# use it to store meta-data about when an avatar gets a promotion
+class Promotion
+  include DataMapper::Resource
+  
+  property :date_promotion, DateTime
+  property :created_at,  DateTime
+  property :updated_at,  DateTime
+  
+  belongs_to :avatar, :key => true
+  belongs_to :grade,  :key => true
+  
+  #has 1, :mission
+end
+
+# Reward model - defines the join between avatars and decorations
+# use it to store meta-data about when an avatar gets a decoration
+class Reward
+  include DataMapper::Resource
+  
+  property :date_decoration, DateTime
+  property :created_at,  DateTime
+  property :updated_at,  DateTime
+  
+  belongs_to :avatar,     :key => true
+  belongs_to :decoration, :key => true
+  
+  has 1, :mission
+end
+
+#####################
+### HELPER MODELS ###
+#####################
+
+class StatutFinMission
+  include DataMapper::Resource
+  property :id,     Serial
+  property :statut, Text, :required => true  
+end
+
+class Revendication
+  include DataMapper::Resource
+  property :id,          Serial
+  property :descriptif , Text, :required => true
+end
+
+class Monture
+  include DataMapper::Resource
+  property :id,          Serial
+  property :modele,      Text, :required => true
+  property :image,       Binary
+  property :specialite,  Integer
+  
+  has 1, :nation
+  belongs_to :flight
+end
+
+class Nation
+  include DataMapper::Resource
+  property :id,        Serial
+  property :pays,      Text, :required => true
+  
+  belongs_to :monture
+  belongs_to :decoration
+  belongs_to :grade
+end
+
+
+
+
 
 
 # call after all models and relationships have been defined
